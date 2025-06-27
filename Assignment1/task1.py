c9224f7bfa40846e8281498567ea4122979e04e0
NaveenVdas <naveenv0912@gmail.com>
2025-06-27 14:04:11 +0530

    Assignment 1

---
+++ Assignment1/task1.py
@@ -0,0 +1,15 @@
+first_num = input("Enter the first number: ")
+first_num = int(first_num)
+
+second_num = int(input("Enter the second number: "))
+
+addition = first_num+second_num
+subtraction = first_num-second_num
+multiplication = first_num * second_num
+division = first_num/second_num
+
+print("\n")
+print("Addition: ",addition)
+print("Subtraction: ",subtraction)
+print("Multiplication: ",multiplication)
+print("Division: ",division)
